4fcfa77c3607d477f0ef3e1877a8842d9327ffb8
asuthamm <asuthamm@gmail.com>
2019-12-20 11:05:05 -0500

    deliverables done

---
+++ app/controllers/appearances_controller.rb
@@ -0,0 +1,21 @@
+class AppearancesController < ApplicationController
+
+
+    # @errors = flash[:errors] 
+    def new
+        @appearance = Appearance.new
+    end
+
+    def create
+        # byebug
+        appearance_params = params.require(:appearance).permit(:rating, :guest_id, :episode_id)
+        appearance = Appearance.create(appearance_params)
+        if appearance.valid?
+            redirect_to episode_path(appearance.episode)
+        else  
+            # flash[:errors] = appearance.errors.full_messages
+            redirect_to new_appearance_path
+        end
+    end
+    
+end
